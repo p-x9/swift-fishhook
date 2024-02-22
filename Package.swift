b4e92bf8bf3be72a79f5b3f891b2d6c16b0e5e9f
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-22 10:26:19 +0900

    bump `MachOKit` version to 0.13.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/p-x9/MachOKit.git", from: "0.9.1")
+        .package(url: "https://github.com/p-x9/MachOKit.git", from: "0.13.0")
     ],
     targets: [
         .target(
